32eb19dfff1eb98bda28a5e45041d2b5352941b5
pedrobomlimeira <33126576+pedrobomlimeira@users.noreply.github.com>
2020-06-11 20:31:01 -0300

    aula 4 parcial 1h24min
    reduzindo os códigos com a utilização de nunjucks

--- src/server.js
+++ src/server.js
@@ -5,15 +5,27 @@ const server = express()
 // configurar pasta publica
 server.use(express.static("public"))
 
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // configurar caminhos da minha aplicaçcao
 // pagina inicial
 // req: requisicao
 // res: resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", {title: "Um título"})
 })
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
